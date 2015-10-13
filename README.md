09b0d6bddabc8eb58328cc5503b3ee3dec37912f
Benno van den Berg <bennovandenberg@gmail.com>
2015-10-13 15:20:22 +0200

    Refactor from glide build to just glide install

---
+++ README.md
@@ -0,0 +1,16 @@
+# glide-install
+
+Uses [glide](https://github.com/Masterminds/glide) to setup a `$GOPATH` and
+install your Go dependencies.
+
+# Example
+
+```yaml
+build:
+    steps:
+         - glide-install
+```
+
+# License
+
+The MIT License (MIT)
--- run.sh
+++ run.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 set -e
 
-# First make sure glide is installed
-if ! type glide &> /dev/null ; then
-  info "glide is not installed, copying binary"
-  sudo cp $WERCKER_STEP_ROOT/glide /usr/bin/glide
-else
-  info "glide is available"
-  debug "glide version: $(glide --version)"
-fi
+debug "$("$WERCKER_STEP_ROOT/glide" --version)"
+info "using GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)"
 
-info "using GOPATH=$(glide gopath)"
-export GOPATH=$(glide gopath)
-glide install
-
-if [ "$WERCKER_GLIDE_BUILD_BUILD" != "false" ]; then
-  go build
-fi
+export GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)
+"$WERCKER_STEP_ROOT/glide" install
