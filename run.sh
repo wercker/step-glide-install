bb061eda32f0538ffa993f0d530208dc689c519b
Benno van den Berg <bennovandenberg@gmail.com>
2016-05-20 14:27:33 +0200

    Update Glide to 0.10.1 (#3)
    * Add .wercker to .gitignore
    * Update to Glide 0.10.2

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-debug "$("$WERCKER_STEP_ROOT/glide" --version)"
-info "using GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)"
+info "Running $("$WERCKER_STEP_ROOT/glide" --version)"
 
-export GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)
 "$WERCKER_STEP_ROOT/glide" install
